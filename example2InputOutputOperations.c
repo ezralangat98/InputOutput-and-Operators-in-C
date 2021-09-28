108d5cd6cbda824b632d4d9207f935eba5ec9cab
Ezra <ezralangat88@gmail.com>
2021-09-28 12:11:24 -0700

    Second Commit

---
+++ example2InputOutputOperations.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+
+    char name[100];
+    int x, y;
+
+    //Prompt for name
+    printf("Enter your name: \n");
+    scanf("%s", &name);
+
+    //Prompting Intro Message
+    printf("Welcome %s!\n", name);
+    printf("Let's do some operations.\n");
+
+    //Prompt for numbers to perform operations on
+    printf("Enter the first number: \n");
+    scanf("%d", &x);
+
+    printf("Enter the second number: \n");
+    scanf("%d", &y);
+
+    //performing operations
+    int modulas = (x%y);
+    int division = (x/y);
+    int additiion = (x+y);
+    int multiplication = (x*y);
+    int subtraction = (x-y);
+
+    //Print the result of each mathematical operation.
+    printf("The addition of %d and %d is %d\n", x, y, additiion);
+    printf("The subtraction of %d and %d is %d\n", x, y, subtraction);
+    printf("The division of %d and %d is %d\n", x, y, division);
+    printf("The modulus of %d and %d is %d\n", x, y, modulas);
+    printf("The multiplication of %d and %d is %d\n", x, y, multiplication);
+
+    return 0;
+}
